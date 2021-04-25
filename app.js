3aaa43afc5e1b46cbc43aac5e209dbbb23682793
AYehia0 <ahmedyehia611@gmail.com>
2021-04-25 14:23:24 +0200

    used the ejs and linked the routes

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const indexRoutes = require("./routes/index");
+const userRoutes = require("./routes/users");
 const app = express();
 
 const PORT = process.env.PORT || 6969
+
+//using the ejs template
+app.use(ejsLayouts);
+//setting the view engine as ejs
+app.set("view engine", "ejs");
+
+
+//routes 
+app.use("/", indexRoutes);
+// users/login or users/register
+app.use("/users", userRoutes);
+
+
+
 
 app.listen(PORT, console.log(`Server running at port ${PORT}`))
