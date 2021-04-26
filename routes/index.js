ea008d1b3e1ecbbaea9421d94aa2dec46ce8103b
AYehia0 <ahmedyehia611@gmail.com>
2021-04-27 01:29:15 +0200

    the dashboard + protection

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 
+// requiring ensureAuthenticated 
+const {ensureAuthenticated} = require("../config/auth");
+
 // Creating a router 
 const router = express.Router();
-
-
 
 router.get("/" ,  (req, res) => {
     res.render("home")
 });
+
+// the dashboard after login
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+    //passing the user
+    res.render("dashboard" , {userName: req.user.username });
+    console.log(res.user.name);
+})
 
 //exporting 
 module.exports = router;
